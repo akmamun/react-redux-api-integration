60830328042883b3170ae71354cac86fd7808f02
Al Mamun Khan <almamunkhan04@gmail.com>
2019-08-26 00:34:33 +0600

    react reduxt middleware init

---
+++ src/reducers/index.js
@@ -0,0 +1,13 @@
+
+function AddTodos(state, action) {
+return {
+    ...state,
+
+    }
+}  
+
+export default function reducer(state = {}, action) {
+const reducers = {
+    'ADD_TODO': AddTodos
+}};
+    
